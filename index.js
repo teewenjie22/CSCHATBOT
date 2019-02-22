8efa5548464fd3428ee96e812065bee81ea81b34
Tee Wen Jie <teewenjie22@gmail.com>
2019-02-22 20:23:58 +0800

    edited new index.js

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const Telegraf = require("telegraf");
-
+console.log("running bot");
 const API_KEY = "718185007:AAFBOS9YuXdkDzEGpD-lnO8-o2UhbOxdB98";
 
 const bot = new Telegraf(API_KEY);
 
-//old syntex
-function greet(name){
-    return "Hello" + name;
-}
+// //old syntex
+// function greet(name){
+//     return "Hello" + name;
+// }
 
-//modern syntax
-const greetagain = (name) => {
-    return "Hello" + name;
-}
+// //modern syntax
+// const greetagain = (name) => {
+//     return "Hello" + name;
+// }
 
-//modern systax allows me to write it shrter with efault return
-const greet2 = name => "Hello" + name;
+// //modern systax allows me to write it shrter with efault return
+// const greet2 = name => "Hello" + name;
 
 const helpHandler = ctx => ctx.reply("Hello");
 bot.help("helpHandler");
 
-bot.help((ctx) => ctx.reply('Hello'))
+// bot.help((ctx) => ctx.reply('Hello'))
 
 bot.launch();
